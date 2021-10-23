9bc886e86a18a0304b2a1648ce677a55a41d2ba4
Mateus Martinez rosa <41523047+mateusmrosa@users.noreply.github.com>
2021-10-23 19:17:44 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Presidente Prudente",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto.png"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular 11 e suas novidades",
+                    Local = "São PAulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                    ImagemURL = "foto1.png"
+                },
+            };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+
+        // [HttpGet("{id}")]
+        // public IEnumerable<Evento> GetById(int id)
+        // {
+        //     return _evento.Where(evento => evento.EventoId == id);
+        // }
+
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de put com id = {id}";
+        }
+
+
+    }
+
+}
