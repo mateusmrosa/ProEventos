23ec9dcbbaca77dcaeaeebd8a6077dbf84b69028
Mateus Martinez rosa <41523047+mateusmrosa@users.noreply.github.com>
2021-10-26 21:26:07 -0300

    V2 Seção 3: Finalizando implementação  EntityFrameWork

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,50 +13,26 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Presidente Prudente",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto.png"
-                },
-                new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular 11 e suas novidades",
-                    Local = "São PAulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                    ImagemURL = "foto1.png"
-                },
-            };
-
-        public EventoController()
+        private readonly DataContext context;
+        public EventoController(DataContext context)
         {
+            this.context = context;
 
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return this.context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return this.context.Eventos.FirstOrDefault(
+                evento => evento.EventoId == id
+            );
         }
-
-
-        // [HttpGet("{id}")]
-        // public IEnumerable<Evento> GetById(int id)
-        // {
-        //     return _evento.Where(evento => evento.EventoId == id);
-        // }
 
 
         [HttpPut("{id}")]
@@ -63,7 +40,6 @@ namespace ProEventos.API.Controllers
         {
             return $"Exemplo de put com id = {id}";
         }
-
 
     }
 
